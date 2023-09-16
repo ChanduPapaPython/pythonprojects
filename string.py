ecd0a3c09e092476d6a2b5d56c48b81a8c29eb3b
chandu <chandudivya09@gmail.com>
2023-09-16 19:13:51 +0530

    string.py

---
+++ string.py
@@ -0,0 +1,10 @@
+message='this is chandu'
+print("/n",message)
+message='this is divya'
+print("/n",message)
+message='heelo'
+print("/n",message)
+message='"yhtgyyu.".ugyg'
+print("/n",message)
+
+
