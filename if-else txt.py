ea573f42af3d9dac4c0391da327193d82e8ff0f9
chandu <chandudivya09@gmail.com>
2023-09-26 18:59:27 +0530

    marks.py

---
+++ if-else txt.py
@@ -0,0 +1,16 @@
+#enter marks alloted in 3 subjects
+
+maths =50
+english =70
+physics =75
+telugu=100
+hindi=50
+chemistry=78
+total =maths+english+physics+telugu+hindi+chemistry
+print("\n the sum of the 6 subject marks are",total)
+avg=total/6
+print("\n the average of 6 subject marks are",avg)
+    
+
+
+
---
+++ marks.py
@@ -0,0 +1,15 @@
+#enter marks alloted in 3 subjects
+
+maths =50
+english =70
+physics =75
+telugu=100
+hindi=50
+chemistry=78
+total =maths+english+physics+telugu+hindi+chemistry
+print("\n english marks are",english)
+print("\n maths marks are",maths)
+print("\n physics marks are",physics)
+print("\n telugu marks are",telugu)
+print("\n hindi marks are",hindi)
+print("\n chemistry marks are",chemistry)
